e567cff40d4bed151746bc2a6cd4ff258eedb843
Elton Carvalho <eltonmc@dcomp.ufs.br>
2017-07-04 10:26:47 -0300

    updated myo_capture_v2.py

--- software/interface/myo_capture_v2.py
+++ software/interface/myo_capture_v2.py
@@ -7,8 +7,8 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
 from PyQt5 import QtCore, QtWidgets
 
-# Serial
-# ser = serial.Serial('/dev/ttyACM0', 115200, timeout=3)  # abre porta serial com o baud rate de 115200
+# serial
+ser = serial.Serial('/dev/ttyACM0', 115200, timeout=3)
 
 class MatplotlibWidget(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -29,8 +29,7 @@ class Main(QtWidgets.QWidget):
         self.showDialog()
         
     def showDialog(self):
-        text, ok = QtWidgets.QInputDialog.getText(self, 'Luke\'s Hand:', 
-            'Número de canais:')
+        text, ok = QtWidgets.QInputDialog.getText(self, 'Luke\'s Hand:', 'Número de canais:')
         
         if ok:
             self.len_ch = int(text)
@@ -39,65 +38,76 @@ class Main(QtWidgets.QWidget):
     def setInterface(self):      
         self.layoutVertical = QtWidgets.QVBoxLayout(self)
         self.matplotlibWidget = []
+        
+        # set n graphs in window
         for i in range(self.len_ch):
             self.matplotlibWidget.append(MatplotlibWidget(self))
             self.layoutVertical.addWidget(self.matplotlibWidget[i])
 
-        self.setGeometry(0, 0, 1920, 1080)
+        # window config
+        self.showMaximized()
         self.setWindowTitle('Graphs')
 
-        # Set var
+        # set var
         self.len_sig = 1000
-        self.data = np.empty(shape=(self.len_ch,self.len_sig)) # len_ch x len_sig
+        self.data = np.empty(shape=(self.len_ch, self.len_sig)) # len_ch x len_sig
         self.data[:][:] = None #NULL
-        self.num_ch, self.num_sig = 0,0
+        self.num_ch, self.num_sig = 0, 0
 
-        # Serial
-        # ser.close()
-        # ser.open()
-        # ser.flushInput()
-        # self.timer = pg.QtCore.QTimer()
-        # self.timer.timeout.connect(self.showSerial)
-        # self.timer.start(0)
+        # serial
+        ser.close()
+        ser.open()
+        ser.flushInput()
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.showSerial)
+        self.timer.start(0)
 
-        self.showSerial()
+        # .txt
+        # self.showSerial()
+        
+        # show window
         self.show()
 
     def showSerial(self):
-        # .txt
-        self.file = open('myo_capture.txt','r') # open file
 
-        for line in self.file:
-            # add data
-            self.data[self.num_ch][self.num_sig] = float(line)
+        # serial
+        while (ser.inWaiting() == 0):
+            pass
+
+        # split string and add data
+        self.num_ch = 0 
+        for sig in ser.readline().rstrip().split(" "):
+            self.data[self.num_ch][self.num_sig] = sig
             self.num_ch += 1
+        self.num_sig += 1
 
-            if self.num_ch >= self.len_ch:
-                self.num_ch, self.num_sig = 0, self.num_sig + 1
+        # plot n graphs
+        if self.num_sig >= self.len_sig:
+            self.num_sig = 0
+            for i in range(self.len_ch):
+                self.matplotlibWidget[i].axis.clear()
+                self.matplotlibWidget[i].axis.plot(self.data[i])
+                self.matplotlibWidget[i].canvas.draw()        
 
-            if self.num_sig >= self.len_sig:
-                self.num_sig = 0
-                for i in range(self.len_ch):
-                    self.matplotlibWidget[i].axis.clear()
-                    self.matplotlibWidget[i].axis.plot(self.data[i])
-                    self.matplotlibWidget[i].canvas.draw()
+        # .txt
+        # self.file = open('myo_capture.txt','r') # open file
 
-        # Serial
-        # while (ser.inWaiting() == 0):
-        #     pass
+        # for line in self.file:
+        #     self.num_ch = 0
+        #     # add data 
+        #     for sig in line.rstrip().split(" "):
+        #         self.data[self.num_ch][self.num_sig] = sig
+        #         self.num_ch += 1
+        #     self.num_sig += 1
 
-        # self.data[self.num_ch][self.num_sig] = float(ser.readline())
-        # self.num_ch += 1
+        #     # plot n graphs
+        #     if self.num_sig >= self.len_sig:
+        #         self.num_sig = 0
+        #         for i in range(self.len_ch):
+        #             self.matplotlibWidget[i].axis.clear()
+        #             self.matplotlibWidget[i].axis.plot(self.data[i])
+        #             self.matplotlibWidget[i].canvas.draw()
 
-        # if self.num_ch >= self.len_ch:
-        #     self.num_ch, self.num_sig = 0, self.num_sig + 1
-
-        # if self.num_sig >= self.len_sig:
-        #     self.num_sig = 0
-        #     for i in range(self.len_ch):
-        #         self.matplotlibWidget[i].axis.clear()
-        #         self.matplotlibWidget[i].axis.plot(self.data[i])
-        #         self.matplotlibWidget[i].canvas.draw()        
 
 if __name__ == '__main__':
     
