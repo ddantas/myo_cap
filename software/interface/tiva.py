b58dc1b0b405a88a51245e9d3efaf98684fc6408
Elton <eltonmc@outlook.com>
2018-05-15 08:09:03 -0300

    feat api tiva

--- software/interface/tiva.py
+++ software/interface/tiva.py
@@ -421,6 +421,49 @@ class SettingsFile():
             else: data = [1000, 0.0, 1.0, 100.0, -2.0, 2.0]
             return data
 
+class ApiTiva():
+    def setBps(self, serial, value):
+        serial.write("B "+ str(value)+"\n")
+        # waiting serial data
+        while serial.inWaiting() == 0:
+            pass
+        packet = serial.readline()
+
+    def setSampleRate(self, serial, value):
+        serial.write("S "+ str(value)+"\n")
+        # waiting serial data
+        while serial.inWaiting() == 0:
+            pass
+        packet = serial.readline()
+
+    def setNChannels(self, serial, value):
+        serial.write("C "+ str(value)+"\n")
+        # waiting serial data
+        while serial.inWaiting() == 0:
+            pass
+        packet = serial.readline()
+
+    def setNBoards(self, serial, value):
+        serial.write("N "+ str(value)+"\n")
+        # waiting serial data
+        while serial.inWaiting() == 0:
+            pass
+        packet = serial.readline()
+
+    def start(self, serial):
+        serial.write("start\n")
+        # waiting serial data
+        while serial.inWaiting() == 0:
+            pass
+        packet = serial.readline()
+
+    def stop(self, serial):
+        serial.write("stop\n")
+        # waiting serial data
+        while serial.inWaiting() == 0:
+            pass
+        packet = serial.readline()
+
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     window = Main()
