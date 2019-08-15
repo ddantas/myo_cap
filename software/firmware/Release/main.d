e93b5e43847cf157a54bf3926fe0ba0e32be1994
isacalenc <isac.silva@dcomp.ufs.br>
2019-08-15 15:29:07 -0300

    new file:   software/firmware/Debug/ccsObjs.opt 	new file:   software/firmware/Debug/emg.map 	new file:   software/firmware/Debug/emg.out 	new file:   software/firmware/Debug/emg_linkInfo.xml 	new file:   software/firmware/Debug/main.d 	new file:   software/firmware/Debug/main.obj 	new file:   software/firmware/Debug/makefile 	new file:   software/firmware/Debug/objects.mk 	new file:   software/firmware/Debug/sources.mk 	new file:   software/firmware/Debug/subdir_rules.mk 	new file:   software/firmware/Debug/subdir_vars.mk 	new file:   software/firmware/Debug/tm4c123gh6pm_startup_ccs.d 	new file:   software/firmware/Debug/tm4c123gh6pm_startup_ccs.obj 	new file:   software/firmware/Debug/uartstdio.d 	new file:   software/firmware/Debug/uartstdio.obj 	new file:   software/firmware/Release/ccsObjs.opt 	new file:   software/firmware/Release/main.d 	new file:   software/firmware/Release/makefile 	new file:   software/firmware/Release/objects.mk 	new file:   software/firmware/Release/sources.mk 	new file:   software/firmware/Release/subdir_rules.mk 	new file:   software/firmware/Release/subdir_vars.mk 	new file:   software/firmware/Release/tm4c123gh6pm_startup_ccs.d 	new file:   software/firmware/Release/tm4c123gh6pm_startup_ccs.obj 	new file:   software/firmware/Release/uartstdio.d 	deleted:    software/firmware/firmware_tiva.zip 	new file:   software/firmware/main.c 	new file:   software/firmware/targetConfigs/Tiva TM4C123GH6PM.ccxml 	new file:   software/firmware/targetConfigs/readme.txt 	new file:   software/firmware/tm4c123gh6pm.cmd 	new file:   software/firmware/tm4c123gh6pm_startup_ccs.c 	modified:   software/interface/.idea/workspace.xml 	modified:   software/interface/config/settings 	modified:   software/interface/leap_cap/src/design_Ui.py 	modified:   software/interface/leap_cap/src/settingsGCS 	modified:   software/interface/tiva.py 	software/interface/data/2019-08-07_15-39-35.log 	software/interface/data/2019-08-07_15-59-09.log 	software/interface/data/2019-08-14_17-09-45.log 	software/interface/data/2019-08-14_17-12-26.log 	software/interface/data/2019-08-14_17-13-45.log 	software/interface/data/2019-08-14_17-14-04.log 	software/interface/data/2019-08-14_17-15-33.log 	software/interface/data/2019-08-14_17-17-54.log 	software/interface/data/2019-08-14_17-21-21.log 	software/interface/data/2019-08-14_17-24-40.log 	software/interface/data/2019-08-14_17-33-35.log 	software/interface/data/2019-08-14_17-34-27.log 	software/interface/data/2019-08-14_17-49-48.log 	software/interface/data/2019-08-14_17-50-03.log 	software/interface/data/2019-08-14_17-55-33.log 	software/interface/data/2019-08-14_17-56-18.log 	software/interface/data/2019-08-14_18-43-30.log 	software/interface/data/2019-08-14_18-49-15.log 	software/interface/data/2019-08-14_18-57-58.log 	software/interface/data/2019-08-14_19-09-51.log 	software/interface/data/2019-08-14_19-11-26.log 	software/interface/data/2019-08-14_19-14-17.log

---
+++ software/firmware/Release/main.d
@@ -0,0 +1,33 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdio.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/_ti_config.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/linkage.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdarg.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdlib.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdbool.h
+main.obj: /home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/string.h
+
+../main.c: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdio.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/_ti_config.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/linkage.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdarg.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_types.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/cdefs.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_types.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdlib.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdint.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/stdint.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/machine/_stdint.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/sys/_stdint.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/stdbool.h: 
+/home/isac/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.1.LTS/include/string.h: 
