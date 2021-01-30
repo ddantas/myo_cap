46ec713edbe9ad70bcc787baab95a657a4bca17b
Feitosa, Asaphe <55762006+FeitosaAsaphe@users.noreply.github.com>
2021-01-29 22:16:20 -0300

    Folder "lib" from the LeapCap was updated with new test files.

---
+++ software/interface/leap_cap/lib/testPyGame.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+pygame.display.set_mode()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit(); #sys.exit() if sys is imported
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                print("Mínimo esquerdo")
+            if event.key == pygame.K_2:
+                print("Anular esquerdo")
+            if event.key == pygame.K_3:
+                print("Médio esquerdo")
+            if event.key == pygame.K_4:
+                print("Indicador esquerdo")
