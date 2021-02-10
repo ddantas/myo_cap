e39ec180ea2cc121c3162b3cd64d99db2ebb08d1
Feitosa, Asaphe <55762006+FeitosaAsaphe@users.noreply.github.com>
2021-02-10 05:12:25 -0300

    Layout class in "PyGameLibExt" it's almost done. Better organization in "PyGameLibExt".
    - Most of methods of Layout class were developed.
    - Some names changed for better clarity.
    - Comments in Layout class were added.

--- software/interface/leap_cap/src/PyGameLibExt.py
+++ software/interface/leap_cap/src/PyGameLibExt.py
@@ -21,47 +21,48 @@ ORIENT_HORIZONTAL = 1
 # Type of elements
 IMAGE             = 0
 PROGRESS_BAR      = 1
-SPACCER           = 2
+SPACER            = 2
 LAYOUT            = 3
 PANEL             = 4
 
+## All "Visual Elements" have the methods: SetSize(size); SetLocalPos(local_pos); GetSize(); GetLocalPos();
+
 ## After, add parameter "visible" in the classes.
 ## Use concat 
-##
 
+## Image Class ##################################################################################################################
 class Image:
     
-    def __init__(self, dimensions, local_pos):
+    def __init__(self, size, local_pos):
         self.type_of_elem  = IMAGE            
-        self.dimensions    = dimensions
+        self.size          = size
         self.local_pos     = local_pos        
 
-
+## Progress Bar Class ###########################################################################################################
 class ProgressBar:
     
     def __init__(self, orient, dimension, local_pos, progress_perc, border_color, bar_color):        
         self.type_of_elem    = PROGRESS_BAR
         # Orientation of the bar
         self.orient          = orient
-        self.outer_dimension = dimension
+        self.outer_size = dimension
         self.outer_local_pos = local_pos        
         self.progress_perc   = progress_perc
         self.border_color    = border_color
-        self.bar_color       = bar_color
-        
-        self.UpdateInnerDim()
+        self.bar_color       = bar_color        
+        self.UpdateInnerSize()
         self.UpdateInnerPos()
 
-    def UpdateInnerDim(self):
+    def UpdateInnerSize(self):
         if(self.orient == ORIENT_VERTICAL):
-            self.inner_dimension = (self.outer_dimension[0]-6, (self.outer_dimension[1]-6) * self.progress_perc )
+            self.inner_size = (self.outer_size[0]-6, (self.outer_size[1]-6) * self.progress_perc )
         # ORIENTAÇÃO HORIZONTAL    
         else:            
-            self.inner_dimension = ((self.outer_dimension[0] - 6) * self.progress_perc, self.outer_dimension[1] - 6)
+            self.inner_size = ((self.outer_size[0] - 6) * self.progress_perc, self.outer_size[1] - 6)
         
     def UpdateInnerPos(self):
         if(self.orient == ORIENT_VERTICAL):            
-            self.inner_local_pos = (self.outer_local_pos[0] + 3, self.outer_local_pos[1] + 3 + ( (self.outer_dimension[1] - 6) - (self.outer_dimension[1] - 6) * self.progress_perc) )
+            self.inner_local_pos = (self.outer_local_pos[0] + 3, self.outer_local_pos[1] + 3 + ( (self.outer_size[1] - 6) - (self.outer_size[1] - 6) * self.progress_perc) )
         # ORIENTAÇÃO HORIZONTAL
         else:            
             self.inner_local_pos = (self.outer_local_pos[0] + 3, self.outer_local_pos[1] + 3)      
@@ -70,37 +71,116 @@ class ProgressBar:
         self.progress_perc = progress_perc   
                 
     def GetDrawParam(self):
-        return ([ self.type_of_elem, self.outer_dimension, self.outer_local_pos,
-                                     self.inner_dimension, self.inner_local_pos,
-                                     self.border_color   , self.bar_color        ])
+        return ([ self.type_of_elem, self.outer_size   , self.outer_local_pos,
+                                     self.inner_size   , self.inner_local_pos,
+                                     self.border_color , self.bar_color        ])
 
+    def SetSize(self, size):       
+        self.outer_size = size
+        self.UpdateInnerSize()
+        
+    def SetLocalPos(self, local_pos):        
+        self.outer_local_pos = local_pos  
+        self.UpdateInnerPos()
+        
+    def GetSize(self):       
+        return self.outer_size
+        
+    def GetLocalPos(self):        
+        return self.outer_local_pos  
         
         
         
-        
-class Spaccer:
+## Spacer Class #################################################################################################################        
+class Spacer:
     
-    def __init__(self, dimensions, local_pos):
-        self.type_of_elem  = SPACCER
-        self.dimensions    = dimensions
+    def __init__(self, size, local_pos):
+        self.type_of_elem  = SPACER
+        self.size          = size
         self.local_pos     = local_pos        
 
+## Layout Class #################################################################################################################
 class Layout:
     
-    def __init__(self):
-        self.type_of_elem  = LAYOUT
-        # visual elements 
-        self.num_tot_elements  = 0
-        self.num_lines         = 0
-        self.num_elem_per_line = []
-        self.elements          = []        
+    def __init__(self, size, local_pos, num_lines, num_colums, list_of_elements, spacer):
+        self.type_of_elem       = LAYOUT 
+        self.size               = size
+        self.local_pos          = local_pos
+        self.num_tot_elements   = num_lines * num_colums 
+        self.num_lines          = num_lines
+        self.num_colums         = num_colums
+        self.num_vert_spacers   = self.CalcNumVertSpacers(num_lines, num_colums)
+        self.num_hor_spacers    = self.CalcNumHorSpacers (num_lines, num_colums)
+        # 2-dim list or grid with "visual elements"
+        self.elements           = [None] * self.num_lines
+        for line in range(self.num_lines):
+            self.elements[line] = [None] * self.num_colums
+        # Atach the Elements in the grid    
+        self.AtachElements( num_lines, num_colums, list_of_elements)            
+        # Set the size of the Elements in the grid    
+        self.SetSizeElements  ( num_lines, num_colums, list_of_elements)
+        # Set the local position of the Elements in the grid    
+        self.SetLocalPosElemnts(self, num_lines, num_colums, list_of_elements)
+        
+    def CalcNumVertSpacers(self, num_lines, num_colums):        
+        # Have zero colums
+        if not num_colums:
+            num_vert_spacers = 0
+            
+        # Have one or more colums
+        else:
+            # Vertical spacers are like vertical bars
+            num_vert_spacers = 1 +  num_colums         
+            
+        return num_vert_spacers        
+    
+    def CalcNumHorSpacers (self, num_lines, num_colums):
+        # Have zero lines
+        if not num_lines:
+            num_hor_spacers = 0
+            
+        # Have one or more lines
+        else:  
+            # Horizontal spacers are like horizontal bars
+            num_hor_spacers = 1 + num_lines
+            
+        return num_hor_spacers
+    
+    # Sets a pointer for each element (object)    
+    def AtachElements(self, num_lines, num_colums, list_of_elements):
+        for line in range(num_lines):
+            for colum in range(num_colums):
+                self.elements[num_lines][num_colums] = list_of_elements[ num_lines + num_colums ]
+    
+    def SetSizeElements(self, num_lines, num_colums, list_of_elements):
+        pass
+    
+    def SetLocalPosElements(self, num_lines, num_colums, list_of_elements):
+        pass
+    
+    # Sets the Layout size        
+    def SetSize(self, size):       
+        self.size = size        
+        
+    # Sets the Layout local position
+    def SetLocalPos(self, local_pos):        
+        self.local_pos = local_pos  
+    
+    # Returns the Layout size    
+    def GetSize(self):       
+        return self.size
+    
+    # Returns the Layout local position    
+    def GetLocalPos(self):        
+        return self.local_pos    
 
+### Panel Class #################################################################################################################
 class Panel:
     
-    def __init__(self, type_of_panel, dimensions, local_pos):
+    def __init__(self, type_of_panel, size, local_pos):
         self.type_of_elem  = PANEL
         self.is_main_panel = type_of_panel
-        self.dimensions    = dimensions
+        self.size          = size
         self.local_pos     = local_pos
         
     #def DrawVisElements(self):
@@ -117,7 +197,7 @@ class Panel:
         
 
         
-    
+### Global Methods ##############################################################################################################    
 def Draw(win, num_elem, elem_draw_param):
     
     for num_elem in range(num_elem):        
