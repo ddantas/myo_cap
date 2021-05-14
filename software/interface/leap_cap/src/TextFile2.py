0ef08baf9d168c6bf2db7628ec696ff618bfa34a
Feitosa, Asaphe <55762006+FeitosaAsaphe@users.noreply.github.com>
2021-05-13 21:41:26 -0300

    TextFile2: New version of the TextFile started to be developed. Please see the description.
    - The new version was putted in the "..\leap_cap_\src" folder to do not break the myocap code.
    - The '2'  in the end of the file it's to differentiate the two versions.
    - A new method "openFile" was developed to open a text file and to load its lines into the corresponding file variablers.

---
+++ software/interface/leap_cap/src/TextFile2.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 13 19:45:40 2021
+
+@author: asaph
+"""
+
+
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import AuxFunctions as AuxFunc
+
+DATA_PATH = 'data/'
+
+class TextFile():
+
+    def __init__(self):
+        # File Parameters
+        self.header = []
+        self.data_header = ''
+        self.data = []
+        
+        # Log Parameters
+        self.id = 0
+        self.line = []
+
+## File Methods ######################################################################################################    
+
+    def writeHeaderLine(self, msg):
+        self.header.append('## %s \n' % (msg))
+    
+    def writeMetadataLine(self, msg, value):
+        self.header.append('# %s: %s \n' % (msg, value))
+
+    def saveFile(self, file_name):
+        try:
+            output = open(DATA_PATH + file_name, 'a')
+            output.writelines(self.header)
+            output.write(str(self.data_header + '\n'))
+            output.writelines(self.data)
+            output.close()
+        except Exception as e:
+           print(e)
+
+    # Method: Opens a text file and classify its lines putting them into the corresponding file variables: header; data_header; data. 
+    #
+    # Input : file_dir_and_name    -> Directory to the text file with the file name.
+    #
+    # Output: None.
+    def openFile(self, file_dir_and_name):
+        # Clear the file parameters.
+        self.header = [];   self.data_header = '';  self.data = []
+        # Char indexes
+        FIRST_CHAR  = 0
+        SECOND_CHAR = 1        
+        try:
+            # Opens the file and iterates through the lines. 
+            with open(file_dir_and_name, 'r') as file:
+                for line in file:
+                    # True if it's a header line.
+                    if line[FIRST_CHAR] == '#' and line[SECOND_CHAR] == '#':    self.header.append(line)                                                                    
+                    # True if it's a data header line.
+                    if line[FIRST_CHAR] == '#' and line[SECOND_CHAR] == ' ':    self.data_header.append(line)
+                    # True if it's a data line.
+                    if line[FIRST_CHAR] != '#' and line[SECOND_CHAR] != '' :    self.data.append(line)                        
+        except:
+            AuxFunc.showMessage('Error!', 'Insert an compatible text file.')
+            
+            
+## Log Methods #######################################################################################################
+
+    def initFile(self, format, name_cols):
+        if len(self.data_header):
+            self.data_header = self.data_header + ';' + name_cols
+        else:
+            self.data_header = '# ' + name_cols
+        self.line[self.id] = []        
+        self.line[self.id].append([])
+        self.line[self.id].append(format)
+        self.line[self.id].append(name_cols)
+        self.id += 1
+        return self.id - 1
+
+    def saveLog(self, id, values):
+        try:
+            self.line[id][0] = values
+            aux = ''
+            for value in self.line[0:]:
+                if(len(aux)):
+                    aux = '%s;%s' % (aux, value[1] % tuple(value[0]))
+                else:
+                    aux = value[1] % tuple(value[0])
+            self.data.append(aux + '\n')
+        except Exception as e:
+            print(e)
+
+    def getLog(self, pos):
+        return np.fromstring(self.data[pos], dtype=np.uint16, sep=';')
+    
+    def getLogLength(self):
+        return len(self.data)
